876685d405a5c7648eed44253e3b154c68aab0b6
Marcos Paulo <marcospaulosousa48@gmail.com>
2024-11-26 10:06:51 -0300

    fix: add new investiment name to database

--- app/_constants/transactions.ts
+++ app/_constants/transactions.ts
@@ -46,7 +46,7 @@ export const TRANSACTION_TYPE_OPTIONS = [
     label: "Depósito",
   },
   {
-    value: TransactionType.INVESTIMENT,
+    value: TransactionType.INVESTMENT,
     label: "Investimento",
   },
 ];
