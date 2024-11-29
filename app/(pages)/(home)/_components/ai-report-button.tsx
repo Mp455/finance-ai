da19f9fb20a6893469c64d872f0219d911739429
Marcos Paulo <marcospaulosousa48@gmail.com>
2024-11-29 11:43:07 -0300

    feat: ensure only premium users can generate ai report

--- app/(pages)/(home)/_components/ai-report-button.tsx
+++ app/(pages)/(home)/_components/ai-report-button.tsx
@@ -15,11 +15,13 @@ import { generateAiReport } from "../_actions/generate-ai-report";
 import { useState } from "react";
 import { ScrollArea } from "@/app/_components/ui/scroll-area";
 import Markdown from "react-markdown";
+import Link from "next/link";
 interface AiReportButtonProps {
   month: string;
+  hasPremiumPlan: boolean;
 }
 
-const AiReportButton = ({ month }: AiReportButtonProps) => {
+const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
   const [report, setReport] = useState<string | null>(null);
   const [reportIsLoading, setReportIsLoading] = useState(false);
 
@@ -41,30 +43,52 @@ const AiReportButton = ({ month }: AiReportButtonProps) => {
           Relatório IA <BotIcon />
         </Button>
       </DialogTrigger>
-      <DialogContent className="max-w-[600px]">
-        <DialogHeader>
-          <DialogTitle>Relatório IA</DialogTitle>
-          <DialogDescription>
-            Use inteligência Artificial para gerar um relatório com insights
-            sobre suas finanças.
-          </DialogDescription>
-        </DialogHeader>
-        <ScrollArea className="prose max-h-[450px] text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
-          <Markdown>{report}</Markdown>
-        </ScrollArea>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant={"ghost"}>Cancelar</Button>
-          </DialogClose>
-          <Button
-            onClick={handleGenerateReportClick}
-            disabled={reportIsLoading}
-          >
-            {reportIsLoading && <Loader2Icon className="animate-spin" />}
-            Gerar relatório
-          </Button>
-        </DialogFooter>
-      </DialogContent>
+      {hasPremiumPlan ? (
+        <DialogContent className="max-w-[600px]">
+          <DialogHeader>
+            <DialogTitle>Relatório IA</DialogTitle>
+            <DialogDescription>
+              Use inteligência Artificial para gerar um relatório com insights
+              sobre suas finanças.
+            </DialogDescription>
+          </DialogHeader>
+          <ScrollArea className="prose max-h-[450px] text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+            <Markdown>{report}</Markdown>
+          </ScrollArea>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant={"ghost"}>Cancelar</Button>
+            </DialogClose>
+            <Button
+              onClick={handleGenerateReportClick}
+              disabled={reportIsLoading}
+            >
+              {reportIsLoading && <Loader2Icon className="animate-spin" />}
+              Gerar relatório
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      ) : (
+        <>
+          <DialogContent className="max-w-[600px]">
+            <DialogHeader>
+              <DialogTitle>Relatório IA</DialogTitle>
+              <DialogDescription>
+                Você precisa de um Plano Premium para gerar relatórios com IA.
+              </DialogDescription>
+            </DialogHeader>
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant={"ghost"}>Cancelar</Button>
+              </DialogClose>
+              <Button asChild>
+                <Link href="/subscription">Assinar Plano Premium</Link>
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </>
+      )}
     </Dialog>
   );
 };
--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 import SummaryCards from "./_components/summary-cards";
@@ -28,8 +28,9 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
     redirect(`?month=${new Date().getMonth() + 1}`);
   }
   const dashboard = await getDashboard(month);
-
   const userCanAddTransaction = await canUserAddTransaction();
+  const user = await clerkClient().users.getUser(userId);
+
   return (
     <>
       <Navbar />
@@ -37,7 +38,12 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex items-center gap-3">
-            <AiReportButton month={month} />
+            <AiReportButton
+              month={month}
+              hasPremiumPlan={
+                user.publicMetadata.subscriptionPlan === "premium"
+              }
+            />
             <TimeSelect />
           </div>
         </div>
